2d06b484a4c4690ec0bea78c3b93d1fd297f5c04
JustinKQuijanoo <quijanju@sheridancollege.ca>
2020-01-16 14:40:45 -0500

    first commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -14,22 +14,20 @@ public class Student {
     private String name;
 
     /**
-     * Get the value of name
-     *
-     * @return the value of name
+     * @return the name
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Set the value of name
-     *
-     * @param name new value of name
+     * @param name the name to set
      */
     public void setName(String name) {
         this.name = name;
     }
+
+    
 
 }
  
